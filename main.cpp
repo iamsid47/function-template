431a0216e258b5ef34ceadfff8295f59df5402ea
iamsid47 <siddheshkulthe43@gmail.com>
2023-01-23 23:07:36 +0530

    created a template

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+template <typename returntyp>
+returntyp funct (returntyp x, returntyp y) {
+	return x + y;
+}
 
-int main(int argc, char** argv) {
+int main() {
+	
 	return 0;
 }
